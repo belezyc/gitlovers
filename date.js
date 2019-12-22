039e88088d6bc7f0b488368bb33544b0aa98ed4c
belezyc <belezyc@yahoo.fr>
2019-12-22 20:16:06 +0100

    Change code date.js

--- date.js
+++ date.js
@@ -1,3 +1,2 @@
 let d = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-let footer = document.getElementById("footer");
-footer.innerHTML = "Today's date is the " + d;
+document.write("Date: " + d);
