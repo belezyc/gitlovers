1072a27c0ba15c8b206b44f28116649e1e04b8b7
belezyc <belezyc@yahoo.fr>
2019-12-22 20:13:38 +0100

    Change footer index.html

--- date.js
+++ date.js
@@ -1,3 +1,3 @@
-let d = new Date();
+let d = new Date().toJSON().slice(0,10).replace(/-/g,'/');
 let footer = document.getElementById("footer");
-footer.innerHTML = "Today's date is " + d;
+footer.innerHTML = "Today's date is the " + d;
